7fd5d6675db2804097291b31702d8e1b129b6590
Priyam Bhowmik <priyam.bhowmik.cs28@iilm.edu>
2026-05-09 15:53:32 +0530

    Update problem_4.py

--- Homework Assignment/Problem: 1-5/problem_4.py
+++ Homework Assignment/Problem: 1-5/problem_4.py
@@ -1,1 +1,11 @@
+class Solution:
+    def findUnion(self, a, b):
 
+        # Python's set union operator does it all in one shot
+        return list(set(a) | set(b))
+
+        # Equivalent explicit version:
+        # result = set()
+        # for x in a: result.add(x)
+        # for x in b: result.add(x)
+        # return list(result)
