d63cd86e960b96190a56995d668bbe572aedd974
Priyam Bhowmik <priyam.bhowmik.cs28@iilm.edu>
2026-05-09 15:56:10 +0530

    Update problem_5.py

--- Homework Assignment/Problem: 1-5/problem_5.py
+++ Homework Assignment/Problem: 1-5/problem_5.py
@@ -1,1 +1,15 @@
+class Solution:
+    def largest(self, arr):
 
+        # Assume first element is the largest
+        max_val = arr[0]
+
+        # Compare with every other element
+        for x in arr[1:]:
+            if x > max_val:
+                max_val = x    # update if a bigger value is found
+
+        return max_val
+
+        # One-liner alternative (uses built-in):
+        # return max(arr)
